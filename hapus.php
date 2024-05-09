0e6442e51e27a5e3dedfa183f24d18bc3a05b239
lauren3090 <lrsam4a1@gmail.com>
2024-05-09 21:15:47 +0700

    first commit

---
+++ hapus.php
@@ -0,0 +1,21 @@
+<?php
+require_once "app/Mhsw.php";
+
+$mhsw = new Mhsw();
+
+// Cek apakah ID data telah diberikan
+if (!isset($_GET['id'])) {
+    echo "ID tidak diberikan.";
+    exit;
+}
+
+$id = $_GET['id'];
+
+if ($mhsw->hapus($id)) {
+    // Redirect ke halaman index.php setelah penghapusan berhasil
+    header("Location: index.php");
+    exit;
+} else {
+    echo "Gagal Menghapus data mahasiswa.";
+}
+?>
